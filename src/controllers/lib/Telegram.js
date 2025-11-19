c15ce300bbd48b6b11c38e3d2e30813f922c1406
Shomalik Davlatov <shomalikdavlatov@gmail.com>
2025-11-19 10:10:00 +0500

    Refactor Telegram.js

--- src/controllers/lib/Telegram.js
+++ src/controllers/lib/Telegram.js
@@ -1,6 +1,7 @@
+// Telegram.js
 import axiosInstance from './axios.js';
 
-const mediaGroups = new Map();
+const OWNER_ID = process.env.OWNER_ID;
 
 function sendMessage(messageObj, messageText) {
     return axiosInstance.get("sendMessage", {
@@ -9,16 +10,46 @@ function sendMessage(messageObj, messageText) {
     });
 }
 
-function sendToAdmin(messageObj) {
-    const messageText = `Name of sender: ${
-        messageObj.from.first_name} ${messageObj.from.last_name ? messageObj.from.last_name : ""
-    }\nUsername: ${messageObj.from.username ? '@' + messageObj.from.username : "-"}\nUser ID: ${
-        messageObj.from.id
-    }\n\nMessage: ${messageObj.text}`;
-    const OWNER_ID = process.env.OWNER_ID;
+function sendToAdmin(messageText, chat_id) {
     return axiosInstance.get("sendMessage", {
         chat_id: OWNER_ID,
         text: messageText,
+    });
+}
+
+async function sendPhotoToAdmin(messageObj) {
+    const photo = messageObj.photo[messageObj.photo.length - 1];
+    const caption = messageObj.caption || "";
+    
+    const messageText = `Name of sender: ${messageObj.from.first_name} ${
+        messageObj.from.last_name ? messageObj.from.last_name : ""
+    }\nUsername: ${messageObj.from.username ? '@' + messageObj.from.username : "-"}\nUser ID: ${
+        messageObj.from.id
+    }\n\nCaption: ${caption}`;
+
+    await axiosInstance.post("sendPhoto", {
+        chat_id: OWNER_ID,
+        photo: photo.file_id,
+        caption: messageText,
+    });
+}
+
+async function sendMediaGroupToAdmin(messageObj) {
+    const messageText = `Name of sender: ${messageObj.from.first_name} ${
+        messageObj.from.last_name ? messageObj.from.last_name : ""
+    }\nUsername: ${messageObj.from.username ? '@' + messageObj.from.username : "-"}\nUser ID: ${
+        messageObj.from.id
+    }\n\nCaption: ${messageObj.caption || ""}`;
+
+    await axiosInstance.get("sendMessage", {
+        chat_id: OWNER_ID,
+        text: messageText,
+    });
+
+    await axiosInstance.post("forwardMessage", {
+        chat_id: OWNER_ID,
+        from_chat_id: messageObj.chat.id,
+        message_id: messageObj.message_id,
     });
 }
 
@@ -28,7 +59,6 @@ function handleMessage(messageObj) {
 
     if (messageText.startsWith("/")) {
         const command = messageText.substring(1);
-
         switch (command) {
             case "start":
                 return sendMessage(
@@ -47,8 +77,14 @@ function handleMessage(messageObj) {
                 );
         }
     } else {
-        if (messageObj.from.id != process.env.OWNER_ID) {
-            sendToAdmin(messageObj);
+        if (messageObj.from.id != parseInt(OWNER_ID)) {
+            const messageText = `Name of sender: ${messageObj.from.first_name} ${
+                messageObj.from.last_name ? messageObj.from.last_name : ""
+            }\nUsername: ${messageObj.from.username ? '@' + messageObj.from.username : "-"}\nUser ID: ${
+                messageObj.from.id
+            }\n\nMessage: ${messageObj.text}`;
+            
+            sendToAdmin(messageText);
             return sendMessage(
                 messageObj,
                 "We got your event post! Admin will check them soon 👀"
@@ -59,57 +95,27 @@ function handleMessage(messageObj) {
 
 async function handleTelegramUpdate(update) {
     if (!update.message) return;
+    
     console.log("Received a message");
     const msg = update.message;
 
     if (msg.media_group_id) {
-        const groupId = msg.media_group_id;
-
-        if (!mediaGroups.has(groupId)) {
-            mediaGroups.set(groupId, {
-                chat_id: msg.chat.id,
-                from_id: msg.from.id,
-                photos: [],
-                caption: null,
-                timeout: null,
-            });
+        if (msg.from.id != parseInt(OWNER_ID)) {
+            await sendMediaGroupToAdmin(msg);
+            await sendMessage(msg, "We got your event post! Admin will check them soon 👀");
         }
-
-        const group = mediaGroups.get(groupId);
-
-        group.photos.push(msg.photo[msg.photo.length - 1].file_id);
-
-        if (msg.caption) group.caption = msg.caption;
-
-        clearTimeout(group.timeout);
-
-        group.timeout = setTimeout(async () => {
-            await finalizeMediaGroup(group);
-            mediaGroups.delete(groupId);
-        }, 5000);
-    } else {
-        handleMessage(msg);
+        return;
     }
-}
+    
+    if (msg.photo && msg.photo.length > 0) {
+        if (msg.from.id != parseInt(OWNER_ID)) {
+            await sendPhotoToAdmin(msg);
+            await sendMessage(msg, "We got your event post! Admin will check them soon 👀");
+        }
+        return;
+    }
 
-async function finalizeMediaGroup(group) {
-    const OWNER_ID = process.env.OWNER_ID;
-
-    const media = group.photos.map((fileId, index) => ({
-        type: "photo",
-        media: fileId,
-        caption: index === 0 ? group.caption || "" : undefined,
-    }));
-
-    await axiosInstance.post("sendMediaGroup", {
-        chat_id: OWNER_ID,
-        media: JSON.stringify(media),
-    });
-
-    await axiosInstance.get("sendMessage", {
-        chat_id: group.chat_id,
-        text: "We got your event post! Admin will check them soon 👀",
-    });
+    await handleMessage(msg);
 }
 
 export { handleTelegramUpdate };
